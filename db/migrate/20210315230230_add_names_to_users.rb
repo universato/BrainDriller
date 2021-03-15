be63dea202feeeada090e0176e79f15ede20f254
universato <universato@gmail.com>
2021-03-16 08:38:09 +0900

    ログイン画面に繋げた

---
+++ db/migrate/20210315230230_add_names_to_users.rb
@@ -0,0 +1,7 @@
+class AddNamesToUsers < ActiveRecord::Migration[6.1]
+  def change
+    add_column :users, :nickname, :string
+    add_column :users, :login_name, :string
+    add_index :users, :login_name
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_15_222701) do
+ActiveRecord::Schema.define(version: 2021_03_15_230230) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -20,7 +20,10 @@ ActiveRecord::Schema.define(version: 2021_03_15_222701) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "nickname"
+    t.string "login_name"
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["login_name"], name: "index_users_on_login_name"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
