71968b9c0217dd8f511bad3a5c6579391c4e512e
universato <universato@gmail.com>
2021-06-21 12:59:55 +0900

    コメントの場所を修正

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   if ENV['HEADED'] || ENV['H']
-    driven_by :selenium, using: :chrome#, screen_size: [1400, 1400]
+    driven_by :selenium, using: :chrome # , screen_size: [1400, 1400]
   else
     driven_by :selenium, using: :headless_chrome
   end
