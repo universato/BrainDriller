2a6b2d686989c5ffd7c086ce951a11c87a7179f6
universato <universato@gmail.com>
2021-07-04 05:19:42 +0900

    Fix Lint/UselessAssignment of RuboCop

--- app/controllers/drills/problems_controller.rb
+++ app/controllers/drills/problems_controller.rb
@@ -4,7 +4,6 @@ class Drills::ProblemsController < ApplicationController
   # problemsとcurernt_use.id でひっかかる UserProblemResultsを持ってくる。
   def index
     drill = Drill.find(params[:drill_id])
-    drill_id = drill.id
 
     if current_user
       @problems = drill.problems.order(created_at: "ASC").includes(:user_problem_results)
