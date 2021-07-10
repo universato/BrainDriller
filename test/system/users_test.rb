a2540c09c3d183307efd3e841bb1c6197bb0279e
universato <universato@gmail.com>
2021-07-10 14:12:27 +0900

    Add test for UsersController#show

--- test/system/users_test.rb
+++ test/system/users_test.rb
@@ -7,4 +7,8 @@ class UsersTest < ApplicationSystemTestCase
   # For login and logout
   # login_as(users(:admin), scope: :user)
   # logout(:user)
+
+  test "show" do
+    visit "/users/#{users(:smith).id}"
+  end
 end
