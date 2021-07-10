9ffff8735a0b5e8d55d9a5586f7326831930f6dd
universato <universato@gmail.com>
2021-07-10 13:20:55 +0900

    Add test for drill_likes

---
+++ test/system/drill_likes_test.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require 'application_system_test_case'
+
+class DrillLikesTest < ApplicationSystemTestCase
+  include Warden::Test::Helpers
+
+  test "solve drill" do
+    login_as(users(:smith), scope: :user)
+
+    visit '/drills'
+    first(".index-drill").click
+    assert_no_text "UnLike"
+    click_button "Like"
+    assert_no_text "Like"
+    click_button "Unlike"
+  end
+end
