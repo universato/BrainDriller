60c08f9102eb7da9d4beb203d500ce0d3f5bc1c9
universato <universato@gmail.com>
2021-07-10 17:33:54 +0900

    Delete Useless actions e.t.c.

--- app/controllers/api/problems_controller.rb
+++ app/controllers/api/problems_controller.rb
@@ -1,14 +1,4 @@
 class API::ProblemsController < ApplicationController
-  def index
-    tsukawareteruno?
-    @drills = Drill.order(created_at: "DESC").page(params[:page])
-    @drills = Drill.order(created_at: "DESC")
-  end
-
-  def show
-    @problem = Problem.find(params[:id])
-  end
-
   def update
     problem = Problem.find(params[:id])
 
