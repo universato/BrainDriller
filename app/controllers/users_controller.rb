a028b90e5154e2e92fd7250c13be416859135b00
universato <universato@gmail.com>
2021-07-10 16:36:18 +0900

    Delete useless controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,4 @@
 class UsersController < ApplicationController
-  def index
-    @users = User.page(params[:page]).per(15)
-  end
-
   def show
     @user = User.find_by!(id: params[:id]) || User.find_by!(login_name: params[:id])
     @problem_user_results = []
