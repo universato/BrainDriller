c2af23f7d739779e1cffa043c816ae4b9848c036
universato <universato@gmail.com>
2021-07-10 18:31:03 +0900

    Delete an useless API action

--- config/routes/api.rb
+++ config/routes/api.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   namespace :api do
     resources :drills, only: %i[index show new create edit]
-    resources :problems, only: %i[update]
     post 'drills/grade', to: "drills#grade"
     get 'mydrills', to: 'drills#mydrills'
   end
