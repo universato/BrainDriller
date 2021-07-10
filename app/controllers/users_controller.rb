9898a33e5eae45fd574d7dcd6e9535a4e17f7270
universato <universato@gmail.com>
2021-07-10 14:05:37 +0900

    404 if no user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find_by(id: params[:id]) || User.find_by(login_name: params[:id])
+    @user = User.find_by!(id: params[:id]) || User.find_by!(login_name: params[:id])
     @problem_user_results = []
 
     @drills = @user.drills
