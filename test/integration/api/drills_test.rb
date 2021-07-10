e361b7b66290bc1ae740d769bef7fa1a1266479b
universato <universato@gmail.com>
2021-07-10 11:56:08 +0900

    Edit drill api test

--- test/integration/api/drills_test.rb
+++ test/integration/api/drills_test.rb
@@ -1,6 +1,8 @@
 require 'test_helper'
 
 class API::DrillsTest < ActionDispatch::IntegrationTest
+  include Warden::Test::Helpers
+
   test 'GET /api/drills.json' do
     json_path = api_drills_path(format: :json)
     assert_equal "/api/drills.json", json_path
@@ -25,6 +27,13 @@ class API::DrillsTest < ActionDispatch::IntegrationTest
     json_path = api_drill_path(drill_id, format: :json)
     assert_equal "/api/drills/#{drill_id}.json", json_path
     get json_path
+    assert_response :ok
+    # OKではないような
+  end
+
+  test 'GET /api/mydrills' do
+    login_as(users(:smith), scope: :user)
+    get '/api/mydrills'
     assert_response :ok
   end
 end
