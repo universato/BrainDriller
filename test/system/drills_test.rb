010355a1ec13da41dd3cadeef43ea9fc5ffc1ff8
universato <universato@gmail.com>
2021-07-10 12:57:43 +0900

    Delete useless methods

--- test/system/drills_test.rb
+++ test/system/drills_test.rb
@@ -4,15 +4,9 @@ require 'application_system_test_case'
 
 class DrillsTest < ApplicationSystemTestCase
   include Warden::Test::Helpers
-  def setup
-  end
-
-  def login_as_basic_member
-    login_as(users(:smith), scope: :user)
-  end
 
   test "create drill" do
-    login_as_basic_member
+    login_as(users(:smith), scope: :user)
 
     visit '/drills/new'
     fill_in "drill-title", with: "Drill Title"
@@ -21,7 +15,7 @@ class DrillsTest < ApplicationSystemTestCase
   end
 
   test "solve drill" do
-    login_as_basic_member
+    login_as(users(:smith), scope: :user)
 
     visit 'drills'
     click_on "Firsts"
--- test/system/users_test.rb
+++ test/system/users_test.rb
@@ -4,18 +4,7 @@ require 'application_system_test_case'
 
 class UsersTest < ApplicationSystemTestCase
   include Warden::Test::Helpers
-  def setup
-  end
-
-  def login_as_admin
-    login_as(users(:admin), scope: :user)
-  end
-
-  def login_as_basic_member
-    login_as(users(:smith), scope: :user)
-  end
-
-  test 'aaa' do
-    login_as_admin
-  end
+  # For login and logout
+  # login_as(users(:admin), scope: :user)
+  # logout(:user)
 end
