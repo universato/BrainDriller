c5ef4f7e845070a3292299423115b77467311653
universato <universato@gmail.com>
2021-07-11 12:40:41 +0900

    Improve script delete_files

--- lib/scripts/delete_files.rb
+++ lib/scripts/delete_files.rb
@@ -1,11 +1,8 @@
 puts path = File.expand_path('../../log/', __dir__)
-
 `rm -rf #{path}`
 `mkdir #{path}`
 `touch #{path + '/.keep'}`
 `touch #{path + '/development.log'}`
 `touch #{path + '/test.log'}`
 
-puts path = File.expand_path('../../tmp/screenshots/', __dir__)
-`rm -rf #{path}`
-`mkdir #{path}`
+`rails tmp:clear`
