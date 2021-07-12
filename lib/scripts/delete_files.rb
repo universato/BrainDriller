5b6c4e6e6e38bebed17b1ba76db658fbec66066c
universato <universato@gmail.com>
2021-07-12 21:34:57 +0900

    Delete script

--- lib/scripts/delete_files.rb
+++
@@ -1,8 +0,0 @@
-puts path = File.expand_path('../../log/', __dir__)
-`rm -rf #{path}`
-`mkdir #{path}`
-`touch #{path + '/.keep'}`
-`touch #{path + '/development.log'}`
-`touch #{path + '/test.log'}`
-
-`rails tmp:clear`
