2292af070c27035958dae47333be7d624a435370
universato <universato@gmail.com>
2021-07-15 06:54:42 +0900

    Add test for password change

--- test/system/users_test.rb
+++ test/system/users_test.rb
@@ -18,4 +18,28 @@ class UsersTest < ApplicationSystemTestCase
     click_link "アカウント", match: :first, exact: true
     click_link "設定", match: :first, exact: true
   end
+
+  test "user change password" do
+    login_as(users(:uni), scope: :user)
+    visit root_path
+    click_link "アカウント", match: :first, exact: true
+    click_link "設定", match: :first, exact: true
+    within 'form[id=edit_user]' do
+      fill_in "user[password]", with: "barbar"
+      fill_in "user[password_confirmation]", with: "barbar"
+      fill_in "user[current_password]", with: "foobar"
+    end
+    click_button "更新する"
+
+    assert_text "アカウント情報を変更しました"
+
+    click_link "アカウント", match: :first, exact: true
+    click_link "設定", match: :first, exact: true
+    within 'form[id=edit_user]' do
+      fill_in "user[password]", with: "foobar"
+      fill_in "user[password_confirmation]", with: "foobar"
+      fill_in "user[current_password]", with: "barbar"
+    end
+    click_button "更新する"
+  end
 end
