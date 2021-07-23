ea47b94f0c277909ba5a9838de787b1b2f13fcbe
universato <universato@gmail.com>
2021-07-23 13:15:27 +0900

    Fix host name

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,7 +68,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
-  host = 'https://brain-driller.herokuapp.com/'
+  host = 'brain-driller.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
