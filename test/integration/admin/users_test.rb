601fbf3f66c0846fd9b234e6913fc3907fddcdc9
universato <universato@gmail.com>
2021-07-25 05:12:47 +0900

    Add test for ACL

--- test/integration/admin/users_test.rb
+++ test/integration/admin/users_test.rb
@@ -5,19 +5,40 @@ class Admin::DrillsTest < ActionDispatch::IntegrationTest
 
   test 'GET /admin/users by normal user' do
     login_as(users(:smith), scope: :user)
+
     get '/admin/users'
+    assert_redirected_to root_url
+
+    get "/admin/users/#{users(:smith).id}"
+    assert_redirected_to root_url
+
+    get "/admin/users/#{users(:smith).login_name}"
     assert_redirected_to root_url
   end
 
   test 'GET /admin/users by logout user' do
     logout(:user)
+
     get '/admin/users'
+    assert_redirected_to root_url
+
+    get "/admin/users/#{users(:smith).id}"
+    assert_redirected_to root_url
+
+    get "/admin/users/#{users(:smith).login_name}"
     assert_redirected_to root_url
   end
 
   test 'GET /admin/users by admin' do
     login_as(users(:admin), scope: :user)
+
     get '/admin/users'
+    assert_response :success
+
+    get "/admin/users/#{users(:smith).id}"
+    assert_response :success
+
+    get "/admin/users/#{users(:smith).login_name}"
     assert_response :success
   end
 end
