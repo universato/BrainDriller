909af9d27b9f2b5c996d0803f178b528f6e5a81a
universato <universato@gmail.com>
2021-07-25 08:52:10 +0900

    Edit drill-guide view

---
+++ db/seeds/add_drill_guide.rb
@@ -0,0 +1,32 @@
+
+
+uni = User.find_by!(login_name: "uni")
+
+ruby_guide = <<MD
+Rubyの基本の問題を集めました。
+Rubyを解いてみるといいです。
+MD
+
+crystal_guide = <<MD
+Rubyとよく似たCrystalですが、Rubyと違う点を集めてみました。
+よかったら解いてみてください。
+MD
+
+javascript_guide = <<MD
+JavaScriptの基本的な問題です。
+どんどん解いてJSを得意になりましょう。
+MD
+
+css_guide = <<MD
+CSSの基本的な問題です。
+解いてみましょう。
+MD
+
+drill = Drill.find_by!(user: uni, title: "Ruby")
+drill.update(guide: ruby_guide)
+
+drill = Drill.find_by!(user: uni, title: "Crystal")
+drill.update(guide: crystal_guide)
+
+drill = Drill.find_by!(user: uni, title: "JavaScript")
+drill.update(guide: javascript_guide)
