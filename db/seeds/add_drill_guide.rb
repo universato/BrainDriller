f03ffb5342f0f4aedc0e218cfe8c5d906a73f155
universato <universato@gmail.com>
2021-07-28 02:15:32 +0900

    Supress RuboCop Alert

--- db/seeds/add_drill_guide.rb
+++ db/seeds/add_drill_guide.rb
@@ -1,23 +1,21 @@
-
-
 uni = User.find_by!(login_name: "uni")
 
-ruby_guide = <<MD
+ruby_guide = <<~MD
 Rubyの基本の問題を集めました。
 Rubyを解いてみるといいです。
 MD
 
-crystal_guide = <<MD
+crystal_guide = <<~MD
 Rubyとよく似たCrystalですが、Rubyと違う点を集めてみました。
 よかったら解いてみてください。
 MD
 
-javascript_guide = <<MD
+javascript_guide = <<~MD
 JavaScriptの基本的な問題です。
 どんどん解いてJSを得意になりましょう。
 MD
 
-css_guide = <<MD
+css_guide = <<~MD
 CSSの基本的な問題です。
 解いてみましょう。
 MD
@@ -30,3 +28,6 @@ drill.update(guide: crystal_guide)
 
 drill = Drill.find_by!(user: uni, title: "JavaScript")
 drill.update(guide: javascript_guide)
+
+drill = Drill.find_by!(user: uni, title: "CSS")
+drill.update(guide: css_guide)
