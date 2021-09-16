63cef1b50803abdc59665f87ab8d2ca25fc7b330
universato <universato@gmail.com>
2021-09-16 16:55:22 +0900

    Remove redundant comments

--- app/models/problem_user_result.rb
+++ app/models/problem_user_result.rb
@@ -5,10 +5,6 @@ class ProblemUserResult < ApplicationRecord
   validates :user_id,    presence: true, numericality: { only_integer: true, greater_than: 0 }
   validates :problem_id, presence: true, numericality: { only_integer: true, greater_than: 0 }
 
-  # 問題の閲覧回数
-  # 問題の回答回数
-  # 問題の正解回数
-  # 問題のカラム名と統一させたい。
   validates :number_of_views, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :number_of_submissions, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :number_of_correct_answers, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
