45e594cf2f38b1906ddf0dec374fad5cf53a7b44
universato <universato@gmail.com>
2021-09-16 17:15:14 +0900

    Delete redundant comments

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,7 @@
 class User < ApplicationRecord
   before_save { self.email = email.downcase }
   before_save { self.login_name = login_name.downcase }
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
   devise(
     :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable, :confirmable,
     :omniauthable, omniauth_providers: [:google_oauth2]
@@ -11,19 +10,10 @@ class User < ApplicationRecord
   has_many :drills
   has_many :drill_likes, class_name: "DrillLike", dependent: :destroy
   has_many :submissions
-  # has_many :problem_user_results, class_name: "ProblemUserResult", foreign_key: "user_id", dependent: :destroy
   has_many :problems, through: :problem_user_results
 
   validates :email, presence: true, uniqueness: { case_sensitive: false }, length: { in: 3..200 }
 
-  # login_name_format = {
-  #   with: /\A[a-z\d](?:[a-z\d]|-(?=[a-z\d]))*\z/i,
-  #   message: 'は半角英数字と-（ハイフン）のみが使用できます 先頭と最後にハイフンを使用することはできません ハイフンを連続して使用することはできません'
-  # }
-  # login_name_format = {
-  #   with: /\A[a-z](?:[a-z]|-(?=[a-z]))*\z/i,
-  #   message: 'は半角の英小文字と半角数字と-（半角ハイフン）のみが使用できます 先頭と最後にハイフンを使用することはできません ハイフンを連続して使用することはできません'
-  # }
   login_name_format = {
     with: /\A[a-z][a-z0-9]*\z/i,
     message: 'は半角の英小文字と数字のみ使用できます。ただし、数字始まりにすることはできません。'
